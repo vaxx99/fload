05bb72dafeb19114ad981ae3a0f3f3766996f136
vaxx99 <vaxx99@gmail.com>
2016-09-07 09:04:57 +0300

    Update 07.09.2016 - logs

--- cnf/cnf.go
+++ cnf/cnf.go
@@ -1,40 +1,40 @@
 package cnf
 
 import (
-    "encoding/json"
-    "io/ioutil"
-    "log"
-    "sync"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"sync"
 )
 
 type Config struct {
-    Term string
-    Port string
-    Path string
+	Term string
+	Port string
+	Path string
 }
 
 var (
-    config     *Config
-    configLock = new(sync.RWMutex)
+	config     *Config
+	configLock = new(sync.RWMutex)
 )
 
 func LoadConfig() {
-    file, err := ioutil.ReadFile("conf.json")
-    if err != nil {
-	log.Fatal("open config: ", err)
-    }
+	file, err := ioutil.ReadFile("conf.json")
+	if err != nil {
+		log.Fatal("open config: ", err)
+	}
 
-    temp := new(Config)
-    if err = json.Unmarshal(file, temp); err != nil {
-	log.Fatal("parse config: ", err)
-    }
-    configLock.Lock()
-    config = temp
-    configLock.Unlock()
+	temp := new(Config)
+	if err = json.Unmarshal(file, temp); err != nil {
+		log.Fatal("parse config: ", err)
+	}
+	configLock.Lock()
+	config = temp
+	configLock.Unlock()
 }
 
 func GetConfig() *Config {
-    configLock.RLock()
-    defer configLock.RUnlock()
-    return config
+	configLock.RLock()
+	defer configLock.RUnlock()
+	return config
 }
--- zstat.go
+++ zstat.go
@@ -245,12 +245,6 @@ func zstat() {
 }
 
 func main() {
-	f, err := os.OpenFile("zstat.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer f.Close()
-	log.SetOutput(f)
 
 	cnf.LoadConfig()
 	cfg = cnf.GetConfig()
